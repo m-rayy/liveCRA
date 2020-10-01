551aa8128fecb81d912f50db108c482cd654e1fa
Emre Gunes <emre.gunes@ing.com>
2020-10-01 08:00:40 +0200

    Updates

---
+++ precache-manifest.13c7f62a7abea6aeef7413e810fcd676.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e8f45f6a9c564fb4984e76a83df805",
+    "url": "/liveCRA/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/liveCRA/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "66730700fac83f941a90",
+    "url": "/liveCRA/static/css/main.a29d1bef.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/liveCRA/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/liveCRA/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66730700fac83f941a90",
+    "url": "/liveCRA/static/js/main.914839cd.chunk.js"
+  },
+  {
+    "revision": "1df9a2bc9f236c15ffcd",
+    "url": "/liveCRA/static/js/runtime-main.c3e63c2f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/liveCRA/static/media/SEGA.12a3db8b.woff"
+  }
+]);
